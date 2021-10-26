e074178748a590d2fc1aed557cd731ef7b31692b
zyz <845229084@qq.com>
2021-10-26 15:45:31 +0800

    2021.10.26

--- src/permission.js
+++ src/permission.js
@@ -57,8 +57,8 @@ router.beforeEach(async(to, from, next) => {
       next()
     } else {
       // other pages that do not have permission to access are redirected to the login page.
-      // next(`/login?redirect=${to.path}`)
-      next(`/login?redirect=/`)
+      next(`/login?redirect=${to.path}`)
+      // next(`/login?redirect=/`)
       NProgress.done()
     }
   }
